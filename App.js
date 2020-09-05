286f97a3a85199cdef60b83413e004af260dcebb
elroychibueze <etoniru.chibueze@everyfarmer.farm>
2020-09-05 01:19:11 +0100

    updated package

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Alert, SafeAreaView, Button } from "react-native";
 
 export default function App() {
+  const handlePress = () => {
+    Alert.alert("So you're here", "Are You a Fool?", [
+      { text: "Yes", onPress: () => console.log("I am a Fool") },
+      { text: "No", onPress: () => console.log("No, but I be fool") },
+    ]);
+  };
+
+  const handlePrompt = () => {
+    Alert.prompt("So you're here", "Are You a Fool?", (text) =>
+      console.log(text)
+    );
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button color="orange" title="Click Mee oh" onPress={handlePrompt} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
