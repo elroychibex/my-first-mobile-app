e8621f8313c0837e93b3d99adf5ebb24e60e9836
elroychibueze <etoniru.chibueze@everyfarmer.farm>
2020-09-18 15:15:48 +0100

    added card component and assets

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import ViewImageScreen from "./app/screens/ViewImageScreen";
 import AppText from "./app/components/AppText";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import AppButton from "./app/components/AppButton";
+import Card from "./app/components/Card";
 
 export default function App() {
   // const handlePress = () => {
@@ -28,7 +29,20 @@ export default function App() {
   //   );
   // };
   return (
-    <WelcomeScreen />
+    <View
+      style={{
+        backgroundColor: "#f8f4f4",
+        padding: 20,
+        paddingTop: 100,
+      }}>
+      <Card
+        title="Red Jacket for sale"
+        subTitle="$100"
+        image={require("./app/assets/jacket.jpg")}
+      />
+    </View>
+
+    // <WelcomeScreen />
 
     // <View
     //   style={{
--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Text, StyleSheet, Platform } from "react-native";
 
-function AppText(props) {
-  return <Text style={styles.text}>{props.children}</Text>;
+function AppText({ children, style }) {
+  return <Text style={[styles.text, style]}>{children}</Text>;
 }
 
 const styles = StyleSheet.create({
   text: {
-    color: "tomato",
     fontSize: 18,
     fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
     ...Platform.select({
