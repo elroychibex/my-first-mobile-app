a96e3ed65ae9907496d5d6829f5fbf00ac20d2ac
elroychibueze <etoniru.chibueze@everyfarmer.farm>
2020-09-18 17:22:28 +0100

    added list details component

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import AppText from "./app/components/AppText";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
   // const handlePress = () => {
@@ -29,18 +30,7 @@ export default function App() {
   //   );
   // };
   return (
-    <View
-      style={{
-        backgroundColor: "#f8f4f4",
-        padding: 20,
-        paddingTop: 100,
-      }}>
-      <Card
-        title="Red Jacket for sale"
-        subTitle="$100"
-        image={require("./app/assets/jacket.jpg")}
-      />
-    </View>
+    <ListingDetailsScreen />
 
     // <WelcomeScreen />
 
